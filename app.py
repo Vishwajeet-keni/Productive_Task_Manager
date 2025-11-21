829b1f8ab6369fffec438a3a171b1763adda9ba0
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2025-11-22 01:26:01 +0530

    Setuped login and register form

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length,ValidationError
 
 app = Flask(__name__)
 
@@ -14,18 +16,38 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder":"Password"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("That username already exists. Please choose a different one.")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder":"Password"})
+    submit = SubmitField("Register")
+
+
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
-    return render_template("register.html")
+    form=RegisterForm()
+    return render_template("register.html",form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    form=LoginForm()
+    return render_template("login.html", form=form)
 
 
 if __name__ == "__main__":
